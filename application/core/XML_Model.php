88b04e2d813475117d1cd8fb8c4d7af5ba85031f
elichu <elichu0@gmail.com>
2018-03-23 13:56:15 -0700

    fixed bug on mtce, new tasks write to xml file

--- application/core/XML_Model.php
+++ application/core/XML_Model.php
@@ -46,13 +46,14 @@ class XML_Model extends Memory_Model
 	 */
 	protected function load()
 	{
-
-		
+                $first = true;
+		$dataindex = 1;
+                $count = 0;
 		if (($tasks = simplexml_load_file($this->_origin)) !== FALSE)
 		{
 			foreach ($tasks as $task) {
 				$record = new stdClass();
-				$record->id = (int) $task->id;
+				$record->id = (int) $task['id'];
 				$record->task = (string) $task->task;
 				$record->priority = (int) $task->priority;
 				$record->size = (int) $task->size;
@@ -62,73 +63,89 @@ class XML_Model extends Memory_Model
 				$record->flag = (int) $task->flag;
 
 				$this->_data[$record->id] = $record;
-			}
+			//}
+		    	if($first){
+			    	foreach ($task as $key => $value) {
+			    		$keyfieldh[] = $key;	
+                                        //echo " key: ".$key." value: ".$value;
+                                        //echo " keyfieldh: " . $keyfieldh[$count];
+			    		//var_dump((string)$value);
+                                        $count++;
+			    	}
+			    	$this->_fields = $keyfieldh;
+			    }
+		    	$first = false; 
 		}
 
 		// rebuild the keys table
 		$this->reindex();
 
 		
-		if (file_exists(realpath($this->_origin))) {
-
-		    $this->xml = simplexml_load_file(realpath($this->_origin));
-		    if ($this->xml === false) {
-			      // error so redirect or handle error
-			      header('location: /404.php');
-			      exit;
-			}
-
-		    $xmlarray =$this->xml;
-
-		    //if it is empty; 
-		    if(empty($xmlarray)) {
-		    	return;
-		    }
-
-		    //get all xmlonjects into $xmlcontent
-		    $rootkey = key($xmlarray);
-		    $xmlcontent = (object)$xmlarray->$rootkey;
-
-		    $keyfieldh = array();
-		    $first = true;
-
-		    //if it is empty; 
-		    if(empty($xmlcontent)) {
-		    	return;
-		    }
-
-		    $dataindex = 1;
-		    $first = true;
-		    foreach ($xmlcontent as $oj) {
-		    	if($first){
-			    	foreach ($oj as $key => $value) {
-			    		$keyfieldh[] = $key;	
-			    		//var_dump((string)$value);
-			    	}
-			    	$this->_fields = $keyfieldh;
-			    }
-		    	$first = false; 
-
-		    	//var_dump($oj->children());
-		    	$one = new stdClass();
-
-		    	//get objects one by one
-		    	foreach ($oj as $key => $value) {
-		    		$one->$key = (string)$value;
-		    	}
-		    	$this->_data[$dataindex++] =$one; 
-		    }	
-
-
-		 	//var_dump($this->_data);
-		} else {
-		    exit('Failed to open the xml file.');
-		}
+//		if (file_exists(realpath($this->_origin))) {
+//
+//		    $this->xml = simplexml_load_file(realpath($this->_origin));
+//		    if ($this->xml === false) {
+//			      // error so redirect or handle error
+//			      header('location: /404.php');
+//			      exit;
+//			}
+//
+//		    $xmlarray =$this->xml;
+//
+//		    //if it is empty; 
+//		    if(empty($xmlarray)) {
+//		    	return;
+//		    }
+//
+//		    //get all xmlonjects into $xmlcontent
+//		    $rootkey = key($xmlarray);
+//		    $xmlcontent = (object)$xmlarray->$rootkey;
+//
+//		    $keyfieldh = array();
+//		    $first = true;
+//
+//		    //if it is empty; 
+//		    if(empty($xmlcontent)) {
+//		    	return;
+//		    }
+//
+//		    $dataindex = 1;
+//		    $first = true;
+//		    foreach ($xmlcontent as $oj) {
+//		    	if($first){
+//			    	foreach ($oj as $key => $value) {
+//			    		$keyfieldh[] = $key;	
+//			    		//var_dump((string)$value);
+//			    	}
+//			    	$this->_fields = $keyfieldh;
+//			    }
+//		    	$first = false; 
+//
+//		    	//var_dump($oj->children());
+//		    	$one = new stdClass();
+//
+//		    	//get objects one by one
+//		    	foreach ($oj as $key => $value) {
+//                            if($key == 'taskid') {
+//                                $one->id = $key['id'];
+//                            } //else {
+//		    		$one->$key = (string)$value;
+//                            //}
+//		    	}
+//		    	$this->_data[$dataindex++] =$one; 
+//		    }	
+//
+//
+//		 	//var_dump($this->_data);
+//		} else {
+//		    exit('Failed to open the xml file.');
+//		}
 
 		// --------------------
 		// rebuild the keys table
 		$this->reindex();
 	}
+        }
 
 	/**
 	 * Store the collection state appropriately, depending on persistence choice.
@@ -136,21 +153,21 @@ class XML_Model extends Memory_Model
 	 */
 	protected function store()
 	{
-		/*
+		
 		// rebuild the keys table
 		$this->reindex();
 		//---------------------
-		*/
+		
 		if (($handle = fopen($this->_origin, "w")) !== FALSE)
 		{
-		/*
+		
 			fputcsv($handle, $this->_fields);
 			foreach ($this->_data as $key => $record)
 				fputcsv($handle, array_values((array) $record));
 			fclose($handle);
 		}
 		// --------------------
-		*/
+		
 		$xmlDoc = new DOMDocument( "1.0");
         $xmlDoc->preserveWhiteSpace = false;
         $xmlDoc->formatOutput = true;
@@ -167,7 +184,5 @@ class XML_Model extends Memory_Model
             $xmlDoc->appendChild($data);
             $xmlDoc->saveXML($xmlDoc);
             $xmlDoc->save($this->_origin);
-		}
-	}
-
+        }
 }
